807da5334092f1dcea448a33c7b757da1162debb
Maik Emanoel <maikemanoel11@gmail.com>
2023-03-24 16:23:01 -0300

    add showOptions function

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const menuSection = document.querySelector('.menu-section')
 const menuToggle = document.querySelector('.menu-toggle')
 const iconsName = document.querySelectorAll('nav ul li p')
 
+const filter = document.querySelector('.filter')
+const filterOptionsGroup = document.querySelector('.filter-options')
+const filterOptions = document.querySelectorAll('.options')
+
 menuToggle.addEventListener('click', activateMenuExpanded)
 function activateMenuExpanded() {
     menuSection.classList.toggle('expanded')
@@ -13,4 +17,17 @@ function activateMenuExpanded() {
             iconName.style.display = 'none'
         }
     })
+}
+
+let isVisible = false
+
+filter.addEventListener('click', showOptions)
+function showOptions() {
+    if(isVisible) {
+        filterOptionsGroup.style.display = 'none'
+        isVisible = false
+    } else {
+        filterOptionsGroup.style.display = 'initial'
+        isVisible = true
+    }
 }
